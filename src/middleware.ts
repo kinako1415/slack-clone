35832c235bed92c4719e5646df3ef9212bf57eac
kinako1415 <Owata09@outlook.jp>
2025-04-06 18:16:45 +0900

    authpageの作成

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,11 +5,11 @@ import {
   nextjsMiddlewareRedirect,
 } from "@convex-dev/auth/nextjs/server";
 
-const isPublicPage = createRouteMatcher(["/signin"]);
+const isPublicPage = createRouteMatcher(["/auth"]);
 
 export default convexAuthNextjsMiddleware((request) => {
-  if (isPublicPage(request) && !isAuthenticatedNextjs()) {
-    return nextjsMiddlewareRedirect(request, "/signin");
+  if (!isPublicPage(request) && !isAuthenticatedNextjs()) {
+    return nextjsMiddlewareRedirect(request, "/auth");
   }
 });
 
